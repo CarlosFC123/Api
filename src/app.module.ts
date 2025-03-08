535a01501a83e68ab9d2b81ddb88cfb51bceea70
CarlosFC123 <carlosbalam496@gmail.com>
2025-03-08 13:10:39 -0600

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,9 +39,9 @@ dotenv.config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres', 
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
+      port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
